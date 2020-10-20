9c31d0ad43990ec3bb97c2b126ef9244eef80eb3
carlosgarciarayo201 <carlos@carlos-VirtualBox>
2020-10-20 13:58:40 +0200

    change

--- src/main/java/com/springboot/app/productos/models/dao/ProductoDao.java
+++ src/main/java/com/springboot/app/productos/models/dao/ProductoDao.java
@@ -2,7 +2,7 @@ package com.springboot.app.productos.models.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.springboot.app.productos.models.entity.Producto;
+import com.app.ms.commons.models.entity.Producto;
 
 public interface ProductoDao extends CrudRepository<Producto, Long>{
 
--- src/main/java/com/springboot/app/productos/models/service/IProductoService.java
+++ src/main/java/com/springboot/app/productos/models/service/IProductoService.java
@@ -2,7 +2,7 @@ package com.springboot.app.productos.models.service;
 
 import java.util.List;
 
-import com.springboot.app.productos.models.entity.Producto;
+import com.app.ms.commons.models.entity.Producto;
 
 public interface IProductoService {
 
